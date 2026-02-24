41f8aaf326a8553c596d93311dd03dc924fc1b59
paperthink <pattaboissxp2010@gmail.com>
2026-02-24 10:32:15 +0530

    completed code

--- delhi.py
+++ delhi.py
@@ -8,12 +8,15 @@ trips = ["Taj Mahal", "Red fort", "GB road"]
 for f in range(0, len(trips)+1):
     
 
-    if os.path.exists(f"Agra/{trips[f-1]}"):
+    if os.path.exists(f"Agra/{trips[f-1]}"):#f-1 = 0 -> 2
 
-        os.rmdir(f"Agra/{trips[f-1]}")
+        os.rmdir(f"Agra/{trips[f-1]}")#so that 3 doesnt get count
+        #it might throw error
+    else:
+        pass
 
-#for m in range(0, 3):
-   # if not os.path.exists(trips[m]):
-    #    os.mkdir(f"Agra/{trips[m]}")
+for m in range(0, 3):
+    if not os.path.exists(trips[m]):
+        os.mkdir(f"Agra/{trips[m]}")
 
 
