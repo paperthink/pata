b597c7e188accd92798422427e532deb4fc78bf6
paperthink <pattaboissxp2010@gmail.com>
2026-02-24 10:28:23 +0530

    enhanced

--- delhi.py
+++ delhi.py
@@ -5,8 +5,15 @@ if not os.path.exists("Agra"):
     os.mkdir("Agra")
 
 trips = ["Taj Mahal", "Red fort", "GB road"]
+for f in range(0, len(trips)+1):
+    
 
-for m in range(0, 3):
-    os.mkdir(f"Agra/{trips[m]}")
+    if os.path.exists(f"Agra/{trips[f-1]}"):
+
+        os.rmdir(f"Agra/{trips[f-1]}")
+
+#for m in range(0, 3):
+   # if not os.path.exists(trips[m]):
+    #    os.mkdir(f"Agra/{trips[m]}")
 
 
